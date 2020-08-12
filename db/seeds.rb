d093b4db6efc41ee9cdb8bd45160d6db599de07d
Léo Pictet <leo@pictet.net>
2020-08-12 12:44:26 +0200

    Defining the Restaurant Controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurant1 = Restaurant.create!(name: "Le Rocher", address: "Rue du Nord Grand Baie, 2", category: "italian" )
+restaurant2 = Restaurant.create!(name: "Supriz", address: "Oberkamp, 43", category: "french" )
+restaurant3 = Restaurant.create!(name: "Le Servan", address: "Rue des Capucines", category: "french" )
+restaurant4 = Restaurant.create!(name: "Frite House", address: "Champ des Reliefs", category: "belgian" )
+restaurant5 = Restaurant.create!(name: "Mario", address: "Boulevard de la merveille", category: "italian" )
