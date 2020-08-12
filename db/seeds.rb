0465a18eaf061478da6cbb864506fd4215954a4c
Léo Pictet <leo@pictet.net>
2020-08-12 14:19:57 +0200

    Controllers Defined

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,4 @@ restaurant2 = Restaurant.create!(name: "Supriz", address: "Oberkamp, 43", catego
 restaurant3 = Restaurant.create!(name: "Le Servan", address: "Rue des Capucines", category: "french" )
 restaurant4 = Restaurant.create!(name: "Frite House", address: "Champ des Reliefs", category: "belgian" )
 restaurant5 = Restaurant.create!(name: "Mario", address: "Boulevard de la merveille", category: "italian" )
+
